1792a10645d1c59a5eabc26f708d692b5d47cf05
mirjalil <vmirjalily@gmail.com>
2014-12-29 23:10:28 -0500

    exhasutive grid search

--- code/classify_NB.exhaustive_GSCV.py
+++ code/classify_NB.exhaustive_GSCV.py
@@ -1,14 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
-#get_ipython().magic(u'load_ext watermark')
-
-#get_ipython().magic(u"watermark -a 'Vahid Mirjalili' -d -p scikit-learn,numpy,numexpr,pandas,matplotlib,plotly -v")
-
-
-# In[2]:
 
 from matplotlib import pyplot as plt
 
@@ -17,51 +7,36 @@ import numpy as np
 import scipy
 import sklearn
 
-#get_ipython().magic(u'matplotlib inline')
 
 
 # ## 1. Read the training and test dataset
 
-# In[3]:
 
 df = pd.read_table('data/labeledTrainData.tsv')
-
 print(df.head())
 
 
-# In[4]:
-
 df_test = pd.read_table('data/testData.tsv')
-
 print(df_test.head())
 
 
 # ### 1.1 Extracting X & y data columns
 
-# In[5]:
-
 data_train = df.loc[:, 'review']
-
 y_train = df.loc[:, 'sentiment']
-
 print(data_train.head())
 
 
-# In[6]:
-
 data_test = df_test.loc[:, 'review']
-
 print(data_test.tail())
 
 
 # ## 2. Text Feature Extraction
 
-# In[7]:
 
 import nltk
 import string
 import re
-from collections import Counter
 
 from nltk.corpus import stopwords
 
@@ -72,9 +47,6 @@ from nltk.corpus import stopwords
 #  **Remove the punctuations**  
 #  **Remove the stopwrods**  
 #  **Tokenize the remaining string**  
-
-# In[8]:
-
 ## For more info, see http://www.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
 
 stemmer = nltk.stem.porter.PorterStemmer()
@@ -102,33 +74,14 @@ def get_tokens(inp_txt):
     return (stems_nopunct)
 
 
-# #### Unit test for tokenizer:
-
-# In[9]:
-
-get_tokens("What's in a name? That which we call a rose by any other name would smell as sweet.")
-
-## Note: you need to download punkt package in nltk:
+## Note: you need to download punkt and stopwords package in nltk:
 # import nltk
 # nltk.download(punkt)
 
 
-# ### 2.2 TF-IDF Feature Extraction
-
-
-# #### Unit test for TF-IDF:
-
-
-# ### 2.3 Evaluate TF-IDF on the reviews
-
-
-# ## 3. Hyper-parameter Optimization using KFold Cross Validation
-
-# In[12]:
 
 from sklearn import pipeline
 from sklearn import metrics
-from sklearn import grid_search
 from sklearn import cross_validation
 from sklearn.naive_bayes import MultinomialNB
 
@@ -136,9 +89,15 @@ import datetime
 import gc # python's garbage collector
 
 
-
-
 # ## 4. Extensive Grid Search
+
+param_dict = {
+    'max_df':
+    'sublinear_tf'
+    'max_features':
+    'ngram_range':
+    'alpha':
+}
 
 
 
@@ -148,6 +107,10 @@ tfidf = sklearn.feature_extraction.text.TfidfVectorizer(
     strip_accents = 'ascii',
     analyzer = 'word',
     smooth_idf = True,
+    max_df = param_dict['maxdf'],
+    sublinear_tf = param_dict['sublinear_tf'],
+    max_features = param_dict['max_features',
+    ngram_range =  param_dict['ngram_range'],
     tokenizer = get_tokens
 )
 
@@ -155,40 +118,49 @@ param_grid = {
     'vect__max_df':[0.2, 0.4, 0.5, 0.6, 0.8],
     'vect__sublinear_tf':[True, False],
     'vect__max_features':[5000, 10000, 20000, 40000, None],
-    'vect__ngram_range':[(1,1), (1,2)],
-    'clf__alpha':[0.1, 0.2, 0.5, 1.0, 2.0, 10.0]
+    'vect__ngram_range':[(1,1), (1,2), (1,3)],
+    'clf__alpha':[0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 10.0]
 }
 
 
 clf_pipe = pipeline.Pipeline([
         ('vect', tfidf),
-        ('clf', MultinomialNB())
+        ('clf', MultinomialNB(alpha = param_dict['alpha']))
 ])
 
 print(clf_pipe.get_params())
 
 auc_scorer = metrics.make_scorer(metrics.roc_auc_score, greater_is_better=True)
 
-grs = grid_search.GridSearchCV(clf_pipe, 
-                  param_grid, 
-                  n_jobs=-1, 
-                  verbose=1,
-                  scoring=auc_scorer,
-                  cv=5
-)
+#grs = grid_search.GridSearchCV(clf_pipe, 
+#                  param_grid, 
+#                  n_jobs=-1, 
+#                  verbose=1,
+#                  scoring=auc_scorer,
+#                  cv=5
+#)
+#
+#grs.fit(data_train, y_train)
+#
+#print("Best score: %0.3f" % grs.best_score_)
+#print("Best parameters set:")
+#best_param = grs.best_estimator_.get_params()
+#for param_name in sorted(param_grid.keys()):
+#    print("\t%s: %r" % (param_name, best_param[param_name]))
 
 
-gc.collect()
 
+cv = cross_validation.StratifiedKFold(y_train, n_folds=5)
+    
+auc_res = 0
+    
+for i, (train_inx, test_inx) in enumerate(cv):
+    model = clf_pipe.fit(data_train[train_inx], y_train[train_inx])
+    pred = model.predict_proba(data_train[test_inx])
+        
+    fpr, tpr, thresh = metrics.roc_curve(y_train[test_inx], pred[:, 1])
+    auc_res += metrics.auc(fpr, tpr)
+        
 
-
-grs.fit(data_train, y_train)
-
-
-
-print("Best score: %0.3f" % grs.best_score_)
-print("Best parameters set:")
-best_param = grs.best_estimator_.get_params()
-for param_name in sorted(param_grid.keys()):
-    print("\t%s: %r" % (param_name, best_param[param_name]))
-
+current_time = datetime.datetime.now().time().isoformat()
+print("Alpha = %.2f   ---->   AUC = %.4f     (%s)" %(param, auc_res/len(cv), current_time))
