1b0eac9c07ebd176b3e62ec68a99cc70f09b70c8
mirjalil <vmirjalily@gmail.com>
2015-01-03 18:16:11 -0500

    py code updated

--- random-forest/classify_forest.py
+++ random-forest/classify_forest.py
@@ -1,9 +1,11 @@
 
+import os
 import pandas as pd
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 
 # Read the labeled training data
-train = pd.read_csv("labeledTrainData.tsv", header=0, \
+train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'labeledTrainData.tsv'), header=0, 
                     delimiter="\t", quoting=3)
 
 
