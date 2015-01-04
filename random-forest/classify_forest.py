558fc8b0df48a148dc71dafa27f1a879b2fa5650
mirjalil <vmirjalily@gmail.com>
2015-01-03 20:06:39 -0500

    py code updated

--- random-forest/classify_forest.py
+++ random-forest/classify_forest.py
@@ -4,6 +4,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 
+from ..word2vec import Word2VecConverter
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import cross_validation
+import pandas as pd
+import numpy as np
+
+
+
 # Read the labeled training data
 train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'labeledTrainData.tsv'), header=0, 
                     delimiter="\t", quoting=3)
